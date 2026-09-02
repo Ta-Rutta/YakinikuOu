d26e5ef736bca8a3e6e1f1e7e7e7bbe94acdd4f0
myonX <ri.myonsan.x@gmail.com>
2026-09-02 18:29:12 +0900

    シーン切り替え

--- OpenSiv3D_0.6.161/Main.cpp
+++ OpenSiv3D_0.6.161/Main.cpp
@@ -1,42 +1,10 @@
 ﻿# include <Siv3D.hpp>
 
+
 // ステートの型は String
 using App = SceneManager<String>;
 
-// タイトルシーン
-class Title : public App::Scene
-{
-public:
-
-	//スタートボタン
-	const Rect start_button{ 400, 300, 200, 100 };
-
-	// コンストラクタ（必ず実装する）
-	Title(const InitData& init)
-		: IScene{ init }
-	{
-
-	}
-
-	// 更新関数
-	void update() override
-	{
-		// 長方形を左クリックしたら
-		if (start_button.leftClicked());
-		{
-			changeScene(U"Title");
-		}
-	}
-
-	// 描画関数
-	void draw() const override
-	{
-		Scene::SetBackground(Palette::White);
-
-		start_button.draw(Palette::Black);
-	}
-};
-
+#include "Title.h"
 void Main()
 {
 	FontAsset::Register(U"TitleFont", FontMethod::MSDF, 48, Typeface::Bold);
