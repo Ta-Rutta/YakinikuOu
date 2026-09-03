65a36bfdd21c3316189da4f7adb81fc116d02ee4
shiroinudoji <sato_shiroinu@icloud.com>
2026-09-04 00:52:28 +0900

    Player Classの作成

---
+++ Player.cpp
@@ -0,0 +1,48 @@
+﻿#include "stdafx.h"
+#include "Player.h"
+
+Player::Player(Vec2 Pos_,double Vel_)
+	: Pos{Pos_}, //位置
+	Vel{Vel_}, //速度
+	PlayerRect{ RectF(Pos,30,10) }
+{
+
+}
+
+void Player::update() {
+	move();
+	drawPlayer();
+}
+
+double Player::getVel() {
+	return Vel; //速度を返す
+}
+
+bool Player::getCollision(const RectF& other) {
+	return PlayerRect.intersects(other); //引数の物体に触れているかの判定を返す
+}
+
+void Player::drawPlayer() {
+	PlayerRect = { Pos,30,10 };
+	PlayerRect.draw(); //プレイヤーの描画
+}
+
+
+
+void Player::move() {
+	if (KeyW.pressed() || KeyUp.pressed()) {
+		Pos.y -= 100*Scene::DeltaTime();
+	}
+	if (KeyS.pressed() || KeyDown.pressed()) {
+		Pos.y += 100 * Scene::DeltaTime();
+	}
+	//上下移動
+
+	/*if (KeyD.pressed() || KeyRight.pressed()) {
+		Pos.x += 1*Scene::DeltaTime();
+	}
+	if (KeyA.pressed() || KeyLeft.pressed()) {
+		Pos.x -= 1 * Scene::DeltaTime();
+	}*/
+	// 左右移動
+}
---
+++ Player.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+class Player
+{
+public:
+	Vec2 Pos; //位置
+	double Vel; //速度
+	RectF PlayerRect; //仮置きのプレイヤー
+
+	Player(Vec2 Pos_,double Vel_); //コンストラクタ
+
+	void update();
+	double getVel(); //速度を返す
+	bool getCollision(const RectF &other); //当たり判定を返す
+	void drawPlayer();
+	void move(); //Playerを動かす処理
+};
+
