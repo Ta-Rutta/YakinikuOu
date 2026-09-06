409c5a77a59b7cd3602a6c3bedde2d80b218f51d
myonX <ri.myonsan.x@gmail.com>
2026-09-06 10:50:03 +0900

    タイトルシーン

--- TitleScene.h
+++ TitleScene.h
@@ -1,8 +1,37 @@
 ﻿#pragma once
+
 using App = SceneManager<String>;
 
-class TitleScene:public App::Scene
+// タイトルシーン
+class TitleScene : public App::Scene
 {
+public:
 
+	//スタートボタン
+	const Rect start_button{ 400, 300, 200, 100 };
+
+	// コンストラクタ（必ず実装する）
+	TitleScene (const InitData& init)
+		: IScene{ init }
+	{
+
+	}
+
+	// 更新関数
+	void update() override
+	{
+		// 長方形を左クリックしたら
+		if (start_button.leftClicked());
+		{
+			changeScene(U"Title");
+		}
+	}
+
+	// 描画関数
+	void draw() const override
+	{
+		Scene::SetBackground(Palette::White);
+
+		start_button.draw(Palette::Black);
+	}
 };
-
