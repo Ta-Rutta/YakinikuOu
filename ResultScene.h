ad6eced4115f14b6fdd5b89353e465753eee43d2
myonX <ri.myonsan.x@gmail.com>
2026-09-06 17:54:08 +0900

    シーンの遷移

--- ResultScene.h
+++ ResultScene.h
@@ -1,1 +1,18 @@
 ﻿#pragma once
+
+using App = SceneManager<String>;
+
+// タイトルシーン
+class ResultScene : public App::Scene
+{
+public:
+
+	//コンストラクタ
+	ResultScene(const InitData& init);
+
+	//更新関数
+	void update() override;
+
+	//描画関数
+	void draw() const override;
+};
