47b8b164337091501527f90f64428cd7e0c01b3c
myonX <ri.myonsan.x@gmail.com>
2026-09-06 11:27:53 +0900

    Speed

--- Speed.cpp
+++ Speed.cpp
@@ -1,2 +1,14 @@
 ﻿#include "stdafx.h"
 #include "Speed.h"
+
+void Speed::speedUp(){
+	speed += accelerate;
+}
+
+void Speed::speedDown(){
+	speed += deceleration;
+}
+
+double Speed::checkSpeed(){
+	return speed;
+}
