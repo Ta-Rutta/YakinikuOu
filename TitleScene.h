bda49b470f3f84e062b537ecf84725954ddce828
myonX <ri.myonsan.x@gmail.com>
2026-09-06 16:59:43 +0900

    ヘッダーファイルとソースファイルを分け分けした

--- TitleScene.h
+++ TitleScene.h
@@ -6,32 +6,15 @@ using App = SceneManager<String>;
 class TitleScene : public App::Scene
 {
 public:
-
 	//スタートボタン
 	const Rect start_button{ 400, 300, 200, 100 };
 
-	// コンストラクタ（必ず実装する）
-	TitleScene (const InitData& init)
-		: IScene{ init }
-	{
+	//コンストラクタ
+	TitleScene(const InitData& init);
 
-	}
+	//更新関数
+	void update() override;
 
-	// 更新関数
-	void update() override
-	{
-		// 長方形を左クリックしたら
-		if (start_button.leftClicked())
-		{
-			changeScene(U"Title");
-		}
-	}
-
-	// 描画関数
-	void draw() const override
-	{
-		Scene::SetBackground(Palette::White);
-
-		start_button.draw(Palette::Black);
-	}
+	//描画関数
+	void draw() const override;
 };
