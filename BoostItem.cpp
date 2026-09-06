6932e22ff50e5690dda2858ebc8a1489ab7ddded
shiroinudoji <sato_shiroinu@icloud.com>
2026-09-06 18:25:51 +0900

    Itemのやつ

--- BoostItem.cpp
+++ BoostItem.cpp
@@ -1,2 +1,24 @@
 ﻿#include "stdafx.h"
 #include "BoostItem.h"
+
+BoostItem::BoostItem(Vec2 pos, Texture tex, std::shared_ptr<Speed> speed)
+	: pos(pos), //描画位置
+	tex(tex),  //テクスチャ
+	speed(speed) //アイテムのスピード
+	hitBox{ RectF(pos, 30, 30) } //当たり判定用 サイズは仮置きです
+{
+
+}
+
+RectF BoostItem::getCollision() {
+	return hitBox; //当たり判定を返す
+}
+
+void BoostItem::draw()const {
+	hitBox(tex).draw(); //描画
+}
+
+void BoostItem::update() {
+	pos.x -= speed->checkSpeed() * Scene::DeltaTime();
+	hitBox = (pos, 30, 30) //位置の更新
+}
