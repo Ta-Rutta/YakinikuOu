73e89693296b4e35035c18ac33e5ac11f8d8ff7f
shiroinudoji <sato_shiroinu@icloud.com>
2026-09-06 11:20:34 +0900

    Player

--- Player.cpp
+++ Player.cpp
@@ -1,9 +1,9 @@
 ﻿#include "stdafx.h"
 #include "Player.h"
 
-Player::Player(Vec2 Pos_, double Vel_)
+Player::Player(Vec2 Pos_)
 	: Pos{ Pos_ }, //位置
-	Vel{ Vel_ }, //速度
+	Vel{ 100 }, //速度
 	PlayerRect{ RectF(Pos,30,10) }
 {
 
@@ -14,9 +14,6 @@ void Player::update() {
 	drawPlayer();
 }
 
-double Player::getVel() {
-	return Vel; //速度を返す
-}
 
 bool Player::getCollision(const RectF& other) {
 	return PlayerRect.intersects(other); //引数の物体に触れているかの判定を返す
@@ -31,18 +28,18 @@ void Player::drawPlayer() {
 
 void Player::move() {
 	if (KeyW.pressed() || KeyUp.pressed()) {
-		Pos.y -= 100 * Scene::DeltaTime();
+		Pos.y -= Vel * Scene::DeltaTime();
 	}
 	if (KeyS.pressed() || KeyDown.pressed()) {
-		Pos.y += 100 * Scene::DeltaTime();
+		Pos.y += Vel * Scene::DeltaTime();
 	}
 	//上下移動
 
 	/*if (KeyD.pressed() || KeyRight.pressed()) {
-		Pos.x += 1*Scene::DeltaTime();
+		Pos.x += Vel*Scene::DeltaTime();
 	}
 	if (KeyA.pressed() || KeyLeft.pressed()) {
-		Pos.x -= 1 * Scene::DeltaTime();
+		Pos.x -= Vel* Scene::DeltaTime();
 	}*/
 	// 左右移動
 }
--- Player.h
+++ Player.h
@@ -6,10 +6,9 @@ private:
 	double Vel; //速度
 	RectF PlayerRect; //仮置きのプレイヤー
 public:
-	Player(Vec2 Pos_, double Vel_); //コンストラクタ
+	Player(Vec2 Pos_); //コンストラクタ
 
 	void update();
-	double getVel(); //速度を返す
 	bool getCollision(const RectF& other); //当たり判定を返す
 	void drawPlayer();
 	void move(); //Playerを動かす処理
