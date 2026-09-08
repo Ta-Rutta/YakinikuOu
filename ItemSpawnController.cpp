80a6f3038e8a110ea5a39e7121a1782a0f7d5dd3
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2026-09-08 17:47:14 +0900

    アイテムの出現間隔

---
+++ ItemSpawnController.cpp
@@ -0,0 +1,15 @@
+﻿#include "stdafx.h"
+#include "ItemSpawnController.h"
+
+void ItemSpawnController::SpawnController(ItemManager& manager, Milage& milage)
+{
+	
+	t += Scene::DeltaTime();
+
+	if (t>spawnTime)
+	{
+		t = 0;
+		manager.addBoostItem();
+		manager.addDownItem();
+	}
+}
---
+++ ItemSpawnController.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "ItemManager.h"
+#include "Milage.h"
+
+class ItemSpawnController
+{
+private:
+	double spawnTime = 1;
+	double t = 0;
+public:
+	void SpawnController(ItemManager& manager, Milage& milage);
+};
+
