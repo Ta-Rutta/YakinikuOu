9dac2c55cbe8da235441991012d5867618791dd6
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2026-09-08 17:46:52 +0900

    竜巻管理クラス

---
+++ TornadeManager.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Tornade.h"
+
+class TornadeManager
+{
+private:
+	Array<std::unique_ptr<Tornade>> tornades;
+
+	RectF DownItemBox;
+
+	std::shared_ptr<Speed> speed;
+
+	const Texture TornadeTexture{ U"Assets/yoshidaViper.png" };
+
+public:
+
+	TornadeManager(std::shared_ptr<Speed> speed);
+
+	void addTornade();
+
+	void deleteTornade();
+
+	void draw()const;
+
+	void update();
+
+};
+
