4fef7b13f65f14cd8e25ce62ea1bb8cc787a2115
shiroinudoji <sato_shiroinu@icloud.com>
2026-09-09 11:56:22 +0900

    ResultSceneの作成

--- ResultScene.h
+++ ResultScene.h
@@ -5,6 +5,16 @@
 class ResultScene : public App::Scene
 {
 public:
+	//Commomから受け取るデータ
+	String name;
+	double milage;
+
+	//今回のスコア表示をする
+	Font score;
+
+	const RectF retry_button{ 100,25,750,550 };
+
+	const RectF title_button{ 100,25,750,575 };
 
 	//コンストラクタ
 	ResultScene(const InitData& init);
@@ -14,4 +24,9 @@ public:
 
 	//描画関数
 	void draw() const override;
+
+	void RetryButton();
+
+	void TitleButton();
+
 };
