67a9e6cc949d3c78132e2642bf3e2447a6066964
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2026-09-11 23:35:47 +0900

    閾値の変更

--- Speed.h
+++ Speed.h
@@ -4,17 +4,17 @@ class Speed
 private:
 	double speed=150;//速さ
 
-	double accelerate=10;//加速量
+	double accelerate=30;//加速量
 
-	double deceleration=-10;//減速量
+	double deceleration=-30;//減速量
 
 	const double minSpeed = 80;//速度の最小値
 
-	const double maxSpeed = 200;//速度の最大値
+	const double maxSpeed = 250;//速度の最大値
 
 	double t = 0;//時間計測
 
-	const double player_game_over_speed = 120;//竜巻を通過できない速度
+	const double player_game_over_speed = 150;//竜巻を通過できない速度
 
 public:
 	void update();
