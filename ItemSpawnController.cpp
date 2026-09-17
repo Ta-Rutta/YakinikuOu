00d5746d5bca4024503f53c45979a522b9a7d83b
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2026-09-18 01:17:28 +0900

    アイテムが出現するレーンと出現グループの設定

--- ItemSpawnController.cpp
+++ ItemSpawnController.cpp
@@ -1,15 +1,48 @@
 ﻿#include "stdafx.h"
 #include "ItemSpawnController.h"
 
-void ItemSpawnController::SpawnController(ItemManager& manager, Milage& milage)
+void ItemSpawnController::SpawnController(ItemManager& manager, Milage& milage,double speed)
 {
 	
 	t += Scene::DeltaTime();
 
+	spawnTime = 1.2-(((speed/500)/5.0));
+
 	if (t>spawnTime)
 	{
+		int ItemAmount = Random(0, 5);
 		t = 0;
-		manager.addBoostItem();
-		manager.addDownItem();
+		std::random_device rd; // 非決定的な乱数生成器
+		std::default_random_engine engine(rd()); // 決定的な乱数生成器
+		std::shuffle(lane.begin(), lane.end(), engine);
+
+		if (ItemAmount == 0)
+		{
+			manager.addBoostItem(lane[0]);
+			manager.addBoostItem(lane[1]);
+			manager.addBoostItem(lane[2]);
+			manager.addBoostItem(lane[3]);
+		}
+	    else if (ItemAmount == 1)
+		{
+			manager.addBoostItem(lane[0]);
+			manager.addDownItem(lane[1]);
+			manager.addDownItem(lane[2]);
+		}
+		else if (ItemAmount == 4 || ItemAmount == 2)
+		{
+			manager.addBoostItem(lane[0]);
+			manager.addDownItem(lane[1]);
+			manager.addDownItem(lane[2]);
+			manager.addDownItem(lane[3]);
+		}
+		else if (ItemAmount == 5 || ItemAmount == 3)
+		{
+			manager.addDownItem(lane[1]);
+			manager.addDownItem(lane[2]);
+			manager.addDownItem(lane[3]);
+		}
+
+		
 	}
 }
--- ItemSpawnController.h
+++ ItemSpawnController.h
@@ -5,9 +5,17 @@
 class ItemSpawnController
 {
 private:
-	double spawnTime = 1;
+	double spawnTime = 1.5;
+
 	double t = 0;
+
+	Array<int> lane{ 200,400,600,800 };
+
+	std::shared_ptr<Speed> speed;
+
 public:
-	void SpawnController(ItemManager& manager, Milage& milage);
+	void SpawnController(ItemManager& manager, Milage& milage,double speed);
+
+	void SpawnItems(int LaneNumber);
 };
 
