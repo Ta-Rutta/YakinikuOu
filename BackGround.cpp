019efbf457990d480edfc08d9c869cb4d8efc6b2
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2026-09-18 01:11:23 +0900

    背景のテクスチャを管理するクラスを追加

---
+++ BackGround.cpp
@@ -0,0 +1,45 @@
+﻿#include "stdafx.h"
+#include "BackGround.h"
+
+BackGround::BackGround(std::shared_ptr<Speed> speed)
+	:speed(speed),
+	small_mountainL(RectF{ Vec2{0,Scene::Height()-500},1920,1200 }),
+	small_mountainR(RectF{ Vec2{Scene::Width(),Scene::Height() - 500},1920,1200 }),
+	big_mountainL(RectF{ Vec2{0,Scene::Height() - 900},1920,1200 }),
+	big_mountainR(RectF{ Vec2{Scene::Width()-10,Scene::Height() - 900},1920,1200 })
+{
+
+}
+
+void BackGround::update()
+{
+	SmallMountainUpdate();
+	BigMountainUpdate();
+}
+
+void BackGround::SmallMountainUpdate()
+{
+	small_mountainL.x -= speed->checkSpeed() * Scene::DeltaTime() * 1.2;
+	small_mountainR.x -= speed->checkSpeed() * Scene::DeltaTime() * 1.2;
+	if (small_mountainL.x <= -Scene::Width()+10)small_mountainL.x = Scene::Width();
+	if(small_mountainR.x <= -Scene::Width()+10)small_mountainR.x = Scene::Width();
+}
+
+void BackGround::BigMountainUpdate()
+{
+	big_mountainL.x -= speed->checkSpeed() * Scene::DeltaTime();
+	big_mountainR.x -= speed->checkSpeed() * Scene::DeltaTime();
+	if (big_mountainL.x <= -Scene::Width()+10)big_mountainL.x = Scene::Width();
+	if (big_mountainR.x <= -Scene::Width()+10)big_mountainR.x = Scene::Width();
+}
+
+void BackGround::draw()const
+{
+	big_mountainL(TextureAsset(U"big_mountain")).draw();
+	big_mountainR(TextureAsset(U"big_mountain")).draw();
+	small_mountainL(TextureAsset(U"small_mountain")).draw();
+	small_mountainR(TextureAsset(U"small_mountain")).draw();
+	sun(TextureAsset(U"sun")).draw();
+	Print << speed->checkSpeed();
+	UIBar.draw(Palette::Lightgreen);
+}
---
+++ BackGround.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+# include "Speed.h"
+
+class BackGround
+{
+private:
+	std::shared_ptr<Speed> speed;
+	RectF small_mountainL;
+	RectF small_mountainR;
+
+	RectF big_mountainL;
+	RectF big_mountainR;
+
+	RectF UIBar{ Vec2{0,Scene::Height() - 200},Scene::Width(),Scene::Height() };
+
+	RectF sun{ Vec2{Scene::Width() - 150,Scene::Height() - 800},100,100 };
+
+	const double textureWidth = Scene::Width();
+
+public:
+	BackGround(std::shared_ptr<Speed> speed);
+
+	void update();
+
+	void SmallMountainUpdate();
+
+	void BigMountainUpdate();
+
+	void draw()const;
+};
+
