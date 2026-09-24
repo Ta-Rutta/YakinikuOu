040ff2ce20eafc0ee29add79bf83c15d8e3b066f
Ta Rutta <tarutta792@gmail.com>
2026-09-24 23:18:26 +0900

    HP管理クラスの実装

---
+++ Hp.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class IHpReadOnly
+{
+public:
+	virtual ~IHpReadOnly() = default;
+
+	virtual int checkHp()const = 0;
+};
+
+class Hp:public IHpReadOnly
+{
+private:
+	int hp;
+
+	int maxHp; //最大Hp
+
+	int minHp; //最少Hp
+
+public:
+	Hp(); //コンストラクタ
+
+	int checkHp()const; //現在のHpを確認
+
+	void hpDown(); //Hp減少メソッド
+
+	void hpUp(); //Hp上昇メソッド
+
+	
+};
+
