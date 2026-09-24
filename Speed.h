fda3aeadb2e3b1aeb876b74bb8abeca26d6a343e
Ta Rutta <tarutta792@gmail.com>
2026-09-24 23:20:46 +0900

    インターフェースを同一ファイル内に記述

--- Speed.h
+++ Speed.h
@@ -1,5 +1,15 @@
 ﻿#pragma once
-#include "ReadOnlySpeed.h"
+
+class ReadOnlySpeed
+{
+public:
+	virtual ~ReadOnlySpeed() = default;
+
+	// 値を取得するメソッドのみを定義 (const メソッド)
+	virtual double checkSpeed()const = 0;
+
+
+};
 
 class Speed:public ReadOnlySpeed
 {
